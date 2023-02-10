2ca1a4e5bb31a9966d4ec3402339e378df7d60a3
iam-ankon <111118106+iam-ankon@users.noreply.github.com>
2023-02-10 18:12:57 +0600

    Add files via upload

---
+++ MainFrame.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public void initialize(User user) {
+        /*************** Info Panel ***************/
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new GridLayout(0, 2, 5, 5));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(30, 50, 30, 50));
+        infoPanel.add(new JLabel("Name:ankon"));
+        
+
+        Component[] labels = infoPanel.getComponents();
+        for (int i = 0; i < labels.length; i++) {
+            labels[i].setFont(new Font("Segoe print", Font.BOLD, 18));
+        }
+
+        add(infoPanel, BorderLayout.NORTH);
+
+
+
+        setTitle("Dashboard");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(1100, 650);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
